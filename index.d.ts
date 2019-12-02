65ff0c6e9fee13294066e8dc36393b355faac2d4
SelvaraniAlagumuthu <selvaqueen@gmail.com>
2019-12-02 09:19:42 +0530

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -9,3 +9,9 @@ export function open(
   path: string,
   options?: RNFileViewerOptions | string
 ): Promise<void>;
+
+export function download(path: string,
+  options?: RNFileViewerOptions | string):Promise<void>;
+
+  export function DownloadZipFile(path: string):Promise<void>;
+  export function UnZipFile():Promise<void>;
